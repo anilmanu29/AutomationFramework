0fb89cf4689e074d1bad62cb28d6f0115f41d1dc
anilkumarb <anilkumarb@CPN-IT-ANILKUMAR.calpionindia.com>
2018-06-14 23:22:32 +0530

    commit changes

--- src/test/java/testcases/BrokerBulkUploadPaymentsmatchedCarrierTest.java
+++ src/test/java/testcases/BrokerBulkUploadPaymentsmatchedCarrierTest.java
@@ -12,6 +12,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import pages.BrokerBulkUploadPaymentsUnmatchedCarrier;
+import pages.BrokerBulkUploadPaymentsmatchedCarrier;
 import pages.BrokerLoginPage;
 import pages.BrokerNewPayment;
 import base.TestBase;
@@ -19,7 +20,7 @@ import base.TestBase;
 public class BrokerBulkUploadPaymentsmatchedCarrierTest extends TestBase 
 {
 	
-	BrokerBulkUploadPaymentsUnmatchedCarrier bbup;
+	BrokerBulkUploadPaymentsmatchedCarrier bbmp;
 	BrokerLoginPage bl;
 	String  payment_status = "Verified";
 	 static String invoice;
@@ -39,7 +40,7 @@ public class BrokerBulkUploadPaymentsmatchedCarrierTest extends TestBase
 		
 		initialization();
 		bl = new BrokerLoginPage();	
-		bbup = new BrokerBulkUploadPaymentsUnmatchedCarrier();
+		bbmp = new BrokerBulkUploadPaymentsmatchedCarrier();
 		al = new ArrayList<String>();
 	}
 	/*-------Initializing driver---------*/
@@ -55,54 +56,54 @@ public class BrokerBulkUploadPaymentsmatchedCarrierTest extends TestBase
 		
 	}
 	@Test(priority = 2)
-	public void verifyBulkUploadPaymentsUnmatched() throws InterruptedException {
-		bbup = new BrokerBulkUploadPaymentsUnmatchedCarrier();
+	public void verifyBulkUploadPaymentsmatched() throws InterruptedException {
+		bbmp = new BrokerBulkUploadPaymentsmatchedCarrier();
 		
 		
 	}
 	
 	@Test( priority = 3)
 	public void verifynewPayment() throws InterruptedException {
-		bbup.newPayment();
+		bbmp.newPayment();
 		Thread.sleep(1000);
 	}
 	
 
 	@Test( priority = 4)
 	public void verifyUploadFile() throws InterruptedException, IOException, AWTException {
-		bbup.UploadFile();
+		bbmp.UploadFile();
 		Thread.sleep(2000);
 	}
 	
 		
-	@Test(dependsOnMethods = { "verifyBulkUploadPaymentsUnmatched" }, priority = 5)
+	@Test(dependsOnMethods = { "verifyBulkUploadPaymentsmatched" }, priority = 5)
 	public void verifyClickimport() throws InterruptedException, IOException {
-		bbup.Clickimport();
+		bbmp.Clickimport();
 		Thread.sleep(2000);
 	}
 	
 	@Test( priority = 6)
 	public void verifyClickschpayment() throws InterruptedException, IOException {
-		bbup.Clickschpayment();
+		bbmp.Clickschpayment();
 		Thread.sleep(1000);
 	}
 		
 	@Test(priority = 7)
 	public void verifyClickGridDown() throws InterruptedException, IOException {
-		bbup.ClickGridDown();
+		bbmp.ClickGridDown();
 		Thread.sleep(1000);
 	}
 	
 	
-	public void verifyBulkUploadPaymentsUnmatchedCarrierElementsDisplayed() {
+	public void verifyBulkUploadPaymentsmatchedCarrierElementsDisplayed() {
 
 		// Verify that the web elements for the Processed tab exist
-		Assert.assertTrue(bbup.lnk_newpayment.isDisplayed(), "newpayment link  not found");
-		Assert.assertTrue(bbup.link_Upload.isDisplayed(), "upload link not found");
-		Assert.assertTrue(bbup.btn_import.isDisplayed(),
+		Assert.assertTrue(bbmp.lnk_newpayment.isDisplayed(), "newpayment link  not found");
+		Assert.assertTrue(bbmp.link_Upload.isDisplayed(), "upload link not found");
+		Assert.assertTrue(bbmp.btn_import.isDisplayed(),
 				"import button not found");
-		Assert.assertTrue(bbup.link_schpaymnt.isDisplayed(), "schedule payment tile not found");
-		Assert.assertTrue(bbup.link_griddown.isDisplayed(), "grid Pulled down column not found");
+		Assert.assertTrue(bbmp.link_schpaymnt.isDisplayed(), "schedule payment tile not found");
+		Assert.assertTrue(bbmp.link_griddown.isDisplayed(), "grid Pulled down column not found");
 		
 
 	}
