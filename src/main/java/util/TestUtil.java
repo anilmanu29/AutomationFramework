c7a9492b2993265a2a5198e94d14de8bb3e1d407
ChristopherVonKaenel <ChristopherVonKaenel@FRG-ChrisKaenel.W2K.TRUCKSTOP.com>
2018-08-02 13:37:18 -0500

    Updated Extent reports to adjust date format and added timezone

--- src/main/java/util/TestUtil.java
+++ src/main/java/util/TestUtil.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -15,20 +18,20 @@ import org.openqa.selenium.TakesScreenshot;
 
 import base.TestBase;
 
-public class TestUtil extends TestBase{
-	
+public class TestUtil extends TestBase {
+
 	public static final long PAGE_LOAD_TIMEOUT = 20;
 	public static final long IMPLICIT_WAIT = 20;
-	public static final String TESTDATA_SHEET_PATH = System.getProperty("user.dir")+ "/src/main/java/com/ra"
-					+ "/qa/testdata/TestData.xlsx";
-	
+	public static final String TESTDATA_SHEET_PATH = System.getProperty("user.dir") + "/src/main/java/com/ra"
+			+ "/qa/testdata/TestData.xlsx";
+
 	Workbook book;
 	Sheet sheet;
 
-	public void switchToFrame(){
+	public void switchToFrame() {
 		driver.switchTo().frame("mainpanel");
 	}
-	
+
 	public Object[][] getTestData(String sheetName) {
 		FileInputStream file = null;
 		try {
@@ -53,23 +56,53 @@ public class TestUtil extends TestBase{
 		}
 		return data;
 	}
-	
+
 	public void takeScreenshotAtEndOfTest() throws IOException {
-		
-		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		String currentDir = System.getProperty("user.dir");
-		
-		FileUtils.copyFile(scrFile, new File(currentDir + "/output/screenshots/" + System.currentTimeMillis() + ".png"));
-		
-		}
-	
-	//this function takes a single string and removes the .0 that Excel likes to add regardless of formatting
-	public static String removeDecimalZeroFormat(String strData)
-	{
-		if(strData.contains(".0"))
-			strData = strData.replace(".0","");
-		
+
+		FileUtils.copyFile(scrFile,
+				new File(currentDir + "/output/screenshots/" + System.currentTimeMillis() + ".png"));
+
+	}
+
+	// this function takes a single string and removes the .0 that Excel likes to
+	// add regardless of formatting
+	public static String removeDecimalZeroFormat(String strData) {
+		if (strData.contains(".0"))
+			strData = strData.replace(".0", "");
+
 		return strData;
+	}
+
+	public static void DateFormatTestExample(String[] args) {
+		Calendar currentDateTime = Calendar.getInstance();
+		System.out.println("---------------------------------------------------");
+		System.out.println("-- Get Year/Month/Day/Hour/Minute using Calendar --");
+		System.out.println("---------------------------------------------------");
+		System.out.println("Year - Calendar [" + currentDateTime.get(Calendar.YEAR) + "]");
+		System.out.println("Month - Calendar [" + currentDateTime.get(Calendar.MONTH) + "]");
+		System.out.println("Day - Calendar [" + currentDateTime.get(Calendar.DAY_OF_MONTH) + "]");
+		System.out.println("Hour - Calendar [" + currentDateTime.get(Calendar.HOUR_OF_DAY) + "]");
+		System.out.println("Minute - Calendar [" + currentDateTime.get(Calendar.MINUTE) + "]");
+		System.out.println("-------------------------------------------------------------------------");
+		System.out.println("-- Get Year/Month/Day/Hour/Minute and Time Zone using SimpleDateFormat --");
+		System.out.println("-------------------------------------------------------------------------");
+		SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+		SimpleDateFormat monthFormat = new SimpleDateFormat("MM");
+		SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
+		SimpleDateFormat hourFormat = new SimpleDateFormat("HH");
+		SimpleDateFormat minuteFormat = new SimpleDateFormat("mm");
+		SimpleDateFormat timeZoneFormat = new SimpleDateFormat("zzz");
+
+		Date currentDate = new Date(currentDateTime.getTimeInMillis());
+		System.out.println("Year - SimpleDateFormat [" + yearFormat.format(currentDate) + "]");
+		System.out.println("Month - SimpleDateFormat [" + monthFormat.format(currentDate) + "]");
+		System.out.println("Day - SimpleDateFormat [" + dayFormat.format(currentDate) + "]");
+		System.out.println("Hour - SimpleDateFormat [" + hourFormat.format(currentDate) + "]");
+		System.out.println("Minute - SimpleDateFormat [" + minuteFormat.format(currentDate) + "]");
+		System.out.println("Time Zone - SimpleDateFormat [" + timeZoneFormat.format(currentDate) + "]");
 	}
 
 }
