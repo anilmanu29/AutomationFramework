2c99df4a5caf18f9b51930e752d79c77133b38b9
ChristopherVonKaenel <ChristopherVonKaenel@FRG-ChrisKaenel.W2K.TRUCKSTOP.com>
2018-08-07 16:28:41 -0500

    Regression Updates - O sprint

--- src/test/java/testcases/loadpay/broker/HandshakewithRTFBrokerTest.java
+++ src/test/java/testcases/loadpay/broker/HandshakewithRTFBrokerTest.java
@@ -10,16 +10,12 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import base.TestBase;
-import pages.loadpay.broker.BrokerDisableCopyPasteConfirmBankAccount;
 import pages.loadpay.broker.BrokerLoginPage;
-import pages.loadpay.broker.BrokerRegisterCanada;
 import pages.loadpay.broker.HandshakewithRTFBroker;
 
 public class HandshakewithRTFBrokerTest extends TestBase {
 
 	BrokerLoginPage brokerloginobj;
-	BrokerRegisterCanada brokerregisterobj;
-	BrokerDisableCopyPasteConfirmBankAccount brokerdisablecopypasteconfirmbankaccountobj;
 	Select typeofentity;
 	HandshakewithRTFBroker handshakertfbrokeobj;
 
@@ -33,8 +29,6 @@ public class HandshakewithRTFBrokerTest extends TestBase {
 	public void setUp() throws AWTException, IOException {
 		initialization();
 		brokerloginobj = new BrokerLoginPage();
-		brokerregisterobj = new BrokerRegisterCanada();
-		brokerdisablecopypasteconfirmbankaccountobj = new BrokerDisableCopyPasteConfirmBankAccount();
 		handshakertfbrokeobj = new HandshakewithRTFBroker();
 		wait = new WebDriverWait(driver, 30);
 	}
@@ -53,23 +47,22 @@ public class HandshakewithRTFBrokerTest extends TestBase {
 		handshakertfbrokeobj.clickLoadPaylink();
 	}
 
-	@Test(dataProvider = "getBrokerLoginData", dependsOnMethods = "verifyNavigationtoLoadpaylink")
-	public void verifyBrokerLogin(String brokeremail, String password) throws InterruptedException, IOException {
+	@Test(dependsOnMethods = "verifyNavigationtoLoadpaylink")
+	public void verifyUnliknMyLoadPayAccount() throws InterruptedException, IOException {
+		handshakertfbrokeobj.clickUnlinkMyLoadPayAccount();
+		Assert.assertTrue(handshakertfbrokeobj.getLoginbutton().isDisplayed(), "Login button NOT found!");
+		Assert.assertTrue(handshakertfbrokeobj.getSignUpTodaybutton().isDisplayed(), "SignUp Today button NOT found!");
 		handshakertfbrokeobj.clickLoginButton();
+
+	}
+
+	@Test(dataProvider = "getBrokerLoginData", dependsOnMethods = "verifyUnliknMyLoadPayAccount")
+	public void verifyBrokerLogin(String brokeremail, String password) throws InterruptedException, IOException {
 		handshakertfbrokeobj.setBrokerUserName(brokeremail);
 		handshakertfbrokeobj.setBrokerPassword(password);
 		handshakertfbrokeobj.clickBrokerLoginButton();
 		Assert.assertTrue(handshakertfbrokeobj.getUnlinkMyLoadPayAccountbutton().isDisplayed(),
 				"Account Succesfully did not hand shake");
-	}
-
-	@Test(dependsOnMethods = "verifyBrokerLogin")
-	public void verifyUnliknMyLoadPayAccount() throws InterruptedException, IOException {
-		handshakertfbrokeobj.clickUnlinkMyLoadPayAccount();
-		Assert.assertTrue(handshakertfbrokeobj.getLoginbutton().isDisplayed(), "Login button NOT found!");
-		Assert.assertTrue(handshakertfbrokeobj.getSignUpTodaybutton().isDisplayed(), "SignUp Today button NOT found!");
-		handshakertfbrokeobj.clickLogOut();
-
 	}
 
 }
