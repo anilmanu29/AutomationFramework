35713198586717768fb0360a8d0efede8a7dced0
ChristopherVonKaenel <ChristopherVonKaenel@FRG-ChrisKaenel.W2K.TRUCKSTOP.com>
2018-12-21 12:44:17 -0600

    Added batch folder and files for regression runs through maven cmds

---
+++ src/main/resources/Batch Files/Loadpay/loadpay_run_SprintN.bat
@@ -0,0 +1,1 @@
+mvn -f C:\AUTOMATION\SELENIUM\_Project\testing\SeleniumFramework clean test -DsuiteFile=C:\AUTOMATION\SELENIUM\_Project\testing\SeleniumFramework\src\main\resources\TestNG_XMLs\LoadPay\2018\Nsprint.xml & allure serve C:\AUTOMATION\SELENIUM\_Project\testing\SeleniumFramework\target\surefire-reports\junitreports
